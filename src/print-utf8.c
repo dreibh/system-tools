f122123bcde86228b23c80f60fa12eaeb7b8afd5
Thomas Dreibholz <dreibh@simula.no>
2024-09-23 17:39:28 +0200

    Properly handle escaped strings.

--- src/print-utf8.c
+++ src/print-utf8.c
@@ -1,4 +1,5 @@
 #define _XOPEN_SOURCE 1
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,10 +28,166 @@ static int consolewidth()
 }
 
 
-// ###### Obtain console printing width of UTF-8 string #####################
-static int stringwidth(const char* utf8String)
+// ###### Digit to number ###################################################
+static unsigned int hexDigitToNumber(const char digit)
 {
-   wchar_t      wide_string[8192];
+   if( (digit >= '0') && (digit <= '9') ) {
+      return (int)digit - (int)'0';
+   }
+   else if( (digit >= 'a') && (digit <= 'f') ) {
+      return 10 + ((int)digit - (int)'a');
+   }
+   else if( (digit >= 'A') && (digit <= 'F') ) {
+      return 10 + ((int)digit - (int)'A');
+   }
+   fprintf(stderr, "hexDigitToNumber: Bad digit %c!\n", digit);
+   exit(1);
+}
+
+
+// ###### Unescape string ###################################################
+static void unescape(const char*  originalString,
+                     char*        unescapedString,
+                     const size_t maxLength)
+{
+   size_t original_string_length = strlen(originalString);
+   if(original_string_length > maxLength - 1) {
+      original_string_length = maxLength - 1;
+   }
+   size_t i = 0;
+   size_t j = 0;
+   while(i < original_string_length) {
+      if(originalString[i] != '\\') {
+         unescapedString[j++] = originalString[i];
+      }
+      else {
+         i++;
+         if(i < original_string_length) {
+            switch(originalString[i]) {
+               case 'n':
+                  unescapedString[j++] = '\n';
+                break;
+               case 'r':
+                  unescapedString[j++] = '\r';
+                break;
+               case '\\':
+                  unescapedString[j++] = '\\';
+                break;
+               case 'x':
+                  if(i + 2 < original_string_length) {
+                    const unsigned int c =
+                        (hexDigitToNumber(originalString[i + 1]) << 4) +
+                        hexDigitToNumber(originalString[i + 2]);
+                     unescapedString[j++] = (char)c;
+                     i += 2;
+                  }
+                break;
+               case '0':
+                  if(i + 3 < original_string_length) {
+                     const unsigned int c =
+                        (hexDigitToNumber(originalString[i + 1]) * 100) +
+                        (hexDigitToNumber(originalString[i + 2]) * 10) +
+                        hexDigitToNumber(originalString[i + 3]);
+                     unescapedString[j++] = (char)c;
+                     i += 3;
+                  }
+                break;
+               case 'u':
+                  if(i + 4 < original_string_length) {
+                    const unsigned int c1 =
+                        (hexDigitToNumber(originalString[i + 1]) << 4) +
+                        hexDigitToNumber(originalString[i + 2]);
+                    const unsigned int c2 =
+                        (hexDigitToNumber(originalString[i + 3]) << 4) +
+                        hexDigitToNumber(originalString[i + 4]);
+                     unescapedString[j++] = (char)c1;
+                     unescapedString[j++] = (char)c2;
+                     i += 4;
+                  }
+                break;
+               case 'U':
+                  if(i + 8 < original_string_length) {
+                    const unsigned int c1 =
+                        (hexDigitToNumber(originalString[i + 1]) << 4) +
+                        hexDigitToNumber(originalString[i + 2]);
+                    const unsigned int c2 =
+                        (hexDigitToNumber(originalString[i + 3]) << 4) +
+                        hexDigitToNumber(originalString[i + 4]);
+                    const unsigned int c3 =
+                        (hexDigitToNumber(originalString[i + 5]) << 4) +
+                        hexDigitToNumber(originalString[i + 6]);
+                    const unsigned int c4 =
+                        (hexDigitToNumber(originalString[i + 7]) << 4) +
+                        hexDigitToNumber(originalString[i + 8]);
+                     unescapedString[j++] = (char)c1;
+                     unescapedString[j++] = (char)c2;
+                     unescapedString[j++] = (char)c3;
+                     unescapedString[j++] = (char)c4;
+                     i += 8;
+                  }
+                break;
+               case 'a':
+                  unescapedString[j++] = '\a';
+                break;
+               case 'b':
+                  unescapedString[j++] = '\b';
+                break;
+               case 'e':
+               case 'E':
+                  unescapedString[j++] = '\e';
+                break;
+               case 'f':
+                  unescapedString[j++] = '\f';
+                break;
+               case 't':
+                  unescapedString[j++] = '\t';
+                break;
+               case 'v':
+                  unescapedString[j++] = '\v';
+                break;
+               default:
+                  unescapedString[j++] = originalString[i];
+                break;
+            }
+         }
+      }
+      i++;
+   }
+   unescapedString[j] = 0x00;
+}
+
+
+// ###### Obtain console printing widthq of UTF-8 string #####################
+static int stringwidth(const char* originalString)
+{
+
+   // ====== Copy string, but filter out ANSI colouring sequences ===========
+   char   utf8String[8192];
+   size_t original_string_length = strlen(originalString);
+   if(original_string_length > sizeof(utf8String) - 1) {
+      original_string_length = sizeof(utf8String) - 1;
+   }
+   bool   inANSISequence = false;
+   size_t j = 0;
+   for(size_t i = 0; i < original_string_length; i++) {
+      if(!inANSISequence) {
+         if(originalString[i] == 0x1b) {
+            inANSISequence = true;
+         }
+         else {
+            utf8String[j++] = originalString[i];
+         }
+      }
+      else {
+         if(isalpha(originalString[i])) {
+            inANSISequence = false;
+         }
+      }
+   }
+   utf8String[j] = 0x00;
+
+   // ====== Get string width ===============================================
+   wchar_t wide_string[sizeof(utf8String)];
    const size_t wide_string_length = mbstowcs((wchar_t*)&wide_string, utf8String,
                                               sizeof(wide_string) / sizeof(wide_string[0]));
    if(wide_string_length < 0) {
@@ -112,9 +269,9 @@ int main (int argc, char** argv)
    };
    enum mode_t  mode        = Indent;
    int          indentWidth = 0;
-   const char*  utf8String  = "";
-   const char*  borderLeft  = NULL;
-   const char*  borderRight = NULL;
+   const char   utf8String[8192];
+   const char   borderLeft[8192];
+   const char   borderRight[8192];
    bool         newline     = false;
    for(int i = 1; i <argc; i++) {
       if( (strcmp(argv[i], "-c") == 0) || (strcmp(argv[i], "--center") == 0) ) {
@@ -139,9 +296,9 @@ int main (int argc, char** argv)
       }
       else if( (strcmp(argv[i], "-s") == 0) || (strcmp(argv[i], "--separator") == 0) ) {
          if(i + 3 < argc) {
-            borderLeft  = argv[i + 1];
-            utf8String  = argv[i + 2];
-            borderRight = argv[i + 3];
+            unescape(argv[i + 1], (char*)&borderLeft,  sizeof(borderLeft));
+            unescape(argv[i + 2], (char*)&utf8String,  sizeof(utf8String));
+            unescape(argv[i + 3], (char*)&borderRight, sizeof(borderRight));
          }
          else {
             fputs("ERROR: Invalid separator setting!\n", stderr);
@@ -158,7 +315,7 @@ int main (int argc, char** argv)
          return 1;
       }
       else {
-         utf8String = argv[i];
+         unescape(argv[i], (char*)&utf8String, sizeof(utf8String));
          break;
       }
    }
@@ -178,7 +335,7 @@ int main (int argc, char** argv)
          separator(borderLeft, utf8String, borderRight);
        break;
       case Width:
-         printf("%d\n", stringwidth(utf8String), utf8String);
+         printf("%d\n", stringwidth(utf8String));
        break;
       case TerminalInfo:
          struct winsize w;
