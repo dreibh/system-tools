24a8ff0bea34d3f33b713f62f4402619ea3172d8
Thomas Dreibholz <dreibh@simula.no>
2025-04-12 09:12:39 +0200

    Added --highlight-params option and updated manpage.

--- src/text-block.c
+++ src/text-block.c
@@ -266,7 +266,7 @@ static void version()
 // ###### Usage #############################################################
 static void usage(const char* program, const int exitCode)
 {
-   fprintf(stderr, "%s %s [-C|--cat] [-0|--discard] [-H|--highlight] [-E|--enumerate] [-X|--extract] [-D|--delete|--remove] [-F|--insert-front insert_file] [-B|--insert-back insert_file] [-R|--replace insert_file] [-i|--input input_file] [-o|--output output_file] [-a|--append] [-b|--begin-tag begin_tag] [-e|--end-tag end_tag] [-y|--include-tags] [-x|--exclude-tags] [-f|--full-tag-lines] [-t|--tags-only] [--highlight-[begin|end|unmarked1|unmarked2|marked1|marked2] label] [--enumerate-format format] [--enumerate-label[1|2] string] [-w|--suppress-warnings] [-h|--help] [-v|--version]\n",
+   fprintf(stderr, "%s %s [-C|--cat] [-0|--discard] [-H|--highlight] [-E|--enumerate] [-X|--extract] [-D|--delete|--remove] [-F|--insert-front insert_file] [-B|--insert-back insert_file] [-R|--replace insert_file] [-i|--input input_file] [-o|--output output_file] [-a|--append] [-b|--begin-tag begin_tag] [-e|--end-tag end_tag] [-y|--include-tags] [-x|--exclude-tags] [-f|--full-tag-lines] [-t|--tags-only] [--highlight-[begin|end|unmarked1|unmarked2|marked1|marked2] label] [--highlight-param begin_label end_label unmarked1_label unmarked2_label marked1_label marked2_label] [--enumerate-format format] [--enumerate-label[1|2] string] [-w|--suppress-warnings] [-h|--help] [-v|--version]\n",
            gettext("Usage:"), program);
    exit(exitCode);
 }
@@ -317,6 +317,7 @@ int main (int argc, char** argv)
       { "highlight-unmarked2", required_argument, 0, 0x2003 },
       { "highlight-marked1",   required_argument, 0, 0x2004 },
       { "highlight-marked2",   required_argument, 0, 0x2005 },
+      { "highlight-params",    required_argument, 0, 0x2006 },
 
       { "suppress-warnings",   no_argument,       0, 'q' },
       { "help",                no_argument,       0, 'h' },
@@ -426,6 +427,26 @@ int main (int argc, char** argv)
           break;
          case 0x2005:
             HighlightMarked2 = optarg;
+          break;
+         case 0x2006:
+            // NOTE: optind already points to the next option here!
+            //       For options with two or more parameters, this
+            //       will be the *second* parameter!
+            if(optind + 4 < argc) {
+               // NOTE: optind points to the *second* parameter here!
+               HighlightBegin     = argv[optind - 1];
+               HighlightEnd       = argv[optind];
+               HighlightUnmarked1 = argv[optind + 1];
+               HighlightUnmarked2 = argv[optind + 2];
+               HighlightMarked1   = argv[optind + 3];
+               HighlightMarked2   = argv[optind + 4];
+               optind += 5;
+            }
+            else {
+               fputs(gettext("ERROR: Invalid arguments for highlight-params!"), stderr);
+               fputs("\n", stderr);
+               return 1;
+            }
           break;
          case 'v':
             version();
