a567522b16e3f835bf928e2afbcf4b3c2907a090
Thomas Dreibholz <dreibh@simula.no>
2025-10-25 10:27:38 +0200

    Some progress.

--- src/Color/text-color.c
+++ src/Color/text-color.c
@@ -1,9 +1,17 @@
+#include <getopt.h>
+#include <libintl.h>
+#include <locale.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <strings.h>
 #include <unistd.h>
-// #include <string.h>
+#ifndef nullptr
+#define nullptr NULL
+#endif
+
+#include "package-version.h"
+
 
 typedef enum
 {
@@ -49,8 +57,8 @@ static int comareColorTableEntries(const void* leftPtr, const void* rightPtr)
 
 // ###### Find entry in color table #########################################
 const ColorTableEntry* colorLookup(const ColorTableEntry* table,
-                                          const unsigned int            entries,
-                                          const char*                   colorName)
+                                   const unsigned int     entries,
+                                   const char*            colorName)
 {
    const ColorTableEntry  key   = { colorName };
    const ColorTableEntry* found =
@@ -79,21 +87,90 @@ const ColorTableEntry* colorLookupX11HTML(const char* htmlColorName)
 }
 
 
+// ###### Version ###########################################################
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 202000L)
+[[ noreturn ]]
+#endif
+static void version()
+{
+   printf("print-color %s\n", SYSTEMTOOLS_VERSION);
+   exit(0);
+}
+
+
+// ###### Usage #############################################################
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 202000L)
+[[ noreturn ]]
+#endif
+static void usage(const char* program, const int exitCode)
+{
+   fprintf(stderr, "%s %s ..."
+           " [-h|--help]"
+           " [-v|--version]\n",
+           gettext("Usage:"), program);
+   exit(exitCode);
+}
+
+
 
 // ###### Main program ######################################################
 int main (int argc, char** argv)
 {
-   const char* name = "goldenrod";
+   const char* foreground = nullptr;
 
-   const ColorTableEntry* entry = colorLookupX11(name);
-   if(entry) {
-      printf("Name: %s \e[38;2;%d;%d;%dm****** This is a test of %s ******\e[0m\n",
-             entry->Name,
-             entry->Value.RGB.Red, entry->Value.RGB.Green, entry->Value.RGB.Blue,
-             entry->Name);
+   // ====== Initialise i18n support ========================================
+   if(setlocale(LC_ALL, "") == nullptr) {
+      setlocale(LC_ALL, "C.UTF-8");   // "C" should exist on all systems!
    }
-   else {
-      puts("not found");
+   bindtextdomain("random-sleep", nullptr);
+   textdomain("random-sleep");
+
+   // ====== Handle arguments ===============================================
+   const static struct option long_options[] = {
+      { "help",    no_argument, 0, 'h' },
+      { "version", no_argument, 0, 'v' },
+      {  nullptr,  0,           0, 0   }
+   };
+
+   int option;
+   int longIndex;
+   while( (option = getopt_long(argc, argv, "hv", long_options, &longIndex)) != -1 ) {
+      switch(option) {
+         case 'v':
+            version();
+          break;
+         case 'h':
+            usage(argv[0], 0);
+          break;
+         default:
+            fprintf(stderr, gettext("ERROR: Invalid argument %s!"), argv[optind - 1]);
+            fputs("\n", stderr);
+            return 1;
+          break;
+      }
+   }
+   if(optind + 1 == argc) {
+      foreground = argv[1];
+   }
+   else if(optind != argc) {
+      usage(argv[0], 1);
+   }
+
+   // ====== Perform configured operations ==================================
+   if(foreground) {
+      printf("Lookup: %s\n", foreground);
+      const ColorTableEntry* entry = colorLookupX11(foreground);
+      if(entry) {
+         printf("Name: %s \e[38;2;%d;%d;%dm****** This is a test of %s ******\e[0m\n",
+               entry->Name,
+               entry->Value.RGB.Red, entry->Value.RGB.Green, entry->Value.RGB.Blue,
+               entry->Name);
+         printf("ANSI Sequence: \\x1b[38;2;%d;%d;%dm\n",
+                entry->Value.RGB.Red, entry->Value.RGB.Green, entry->Value.RGB.Blue);
+      }
+      else {
+         puts("not found");
+      }
    }
 
    return 0;
