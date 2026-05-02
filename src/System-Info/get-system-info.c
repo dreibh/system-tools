68474db98a2035a10ee8c1679aa6fdbe1aa61596
Thomas Dreibholz <dreibh@simula.no>
2026-05-02 15:46:46 +0200

    FreeBSD fix.

--- src/System-Info/get-system-info.c
+++ src/System-Info/get-system-info.c
@@ -563,21 +563,20 @@ static bool obtainDiskUsage(const char* mountPoint, const char* label)
    // ====== Compute the size statistics ====================================
    // Computations according to "df" command's output:
    const unsigned long long totalSpace =
-      (unsigned long long)status.f_blocks * (unsigned long long)status.f_bsize;
+      (unsigned long long)status.f_blocks * (unsigned long long)status.f_frsize;
    const unsigned long long availableSpace =   /* available for user */
-      (unsigned long long)status.f_bavail * (unsigned long long)status.f_bsize;
+      (unsigned long long)status.f_bavail * (unsigned long long)status.f_frsize;
    const unsigned long long freeSpace =   /* actual free space */
-      (unsigned long long)status.f_bfree * (unsigned long long)status.f_bsize;
+      (unsigned long long)status.f_bfree * (unsigned long long)status.f_frsize;
    const unsigned long long usedSpace = totalSpace - freeSpace;
    const double usagePercentage = 100.0 *
       (double)(totalSpace - availableSpace) / (double)totalSpace;
 
    // ====== Print the results ==============================================
    printf("disk_%s_total=%llu\n",     label, totalSpace);
-   printf("disk_%s_available=%llu\n", label, availableSpace);
    printf("disk_%s_used=%llu\n",      label, usedSpace);
+   printf("disk_%s_available=%llu\n", label, availableSpace);
    printf("disk_%s_pct=%1.3f\n",      label, usagePercentage);
-
    return true;
 }
 
